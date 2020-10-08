965c0c3e710951e09509938e9559a6843e760a7b
Zenzi Ali <zeeathome935@gmail.com>
2020-10-08 16:02:42 -0400

    updated error handler

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -1,9 +1,11 @@
-const { NODE_ENV } = require('../config')
+const { NODE_ENV } = require("../config");
 
 module.exports = function errorHandler(error, req, res, next) {
-  const response = (NODE_ENV === 'production')
-    ? { error: 'Server error' }
-    : (console.error(error), { error: error.message, details: error })
+  const response =
+    NODE_ENV === "production"
+      ? { error: "Server error" }
+      : { error: error.message, details: error };
+  console.error(error);
 
-  res.status(500).json(response)
-}
+  res.status(500).json(response);
+};
